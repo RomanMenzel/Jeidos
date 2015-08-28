e357065733980d113dd891333fa502ee6bb34b5a
Roman Menzel <roman.menzel1@gmx.net>
2015-08-28 20:06:08 +0200

    Update pw.rb

--- pw.rb
+++ pw.rb
@@ -1,11 +1,11 @@
 #Password generator
 
-@@pwFiles = [",", ".", "?", "A", "B", "C", "D", "E", ")", "(", "#", "!", "%", "&", "$", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", ">", "<", "a", "u", "o", "+", "m", "z", "=", "_"]
+pwFiles = [",", ".", "?", "A", "B", "C", "D", "E", ")", "(", "#", "!", "%", "&", "$", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", ">", "<", "a", "u", "o", "+", "m", "z", "=", "_"]
 
 def gen_pw
     $password = String.new
     8.times do
-        password = @@pwFiles[rand(@@pwFiles.size)]
+        password = pwFiles[rand(pwFiles.size)]
         $password << password
     end
     return $password
